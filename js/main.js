74ac86ffc812530e391ef4358352d9473d7acc39
noorjallad99 <48832632+noorjallad99@users.noreply.github.com>
2021-11-22 19:47:06 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// var x= Number(window.prompt("Enter your number!"));
+// if(x%2==0)
+// window.alert("x is even");
+// else 
+// window.alert("x is odd");
+
+// document.getElementById("demo").innerHTML='Hello';
+
+// if(x%3!=0 && x%4==0)
+// window.alert("yes");
+// else
+// window.alert("no");
+// var x2=10;
+// var y=10;
+// var result=x2+y;
+// if(x==y)
+// window.alert("ok");
+// else
+// window.alert("not ok");
+// ===   =>>> بتفحص القيمة و الداتا تايب
+//== >> بتفحص القيم .. حتى لو اختلف النوع للقيم المدخلة
+
+var career=window.prompt("Enter your career");
+if(career=="frontend-developer")
+window.alert("U should learn angular");
+else 
+if(career=="backend-developer")
+window.alert("U should learn laravel");
+else 
+if(career=="IOS-developer")
+window.alert("U should learn swift");
+else
+window.alert("error");
+
